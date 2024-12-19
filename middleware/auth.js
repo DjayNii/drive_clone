4e35aefd58abcbd153673a082177f9bf7442e946
DjayNii <nithinreddyk24@gmail.com>
2024-12-19 21:13:44 +0530

    added error and auth.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,38 @@
 //this file is used for authentication
 
 const jwt = require("jsonwebtoken");
+
+// we will make a custom middle ware for authentication which will tell if he is logged in or not
+
+function auth(req, res, next) {
+  const token = req.cookies.token; // we call the token which is stored in cokkie
+
+  if (!token) {
+    return res.status(401).json({
+      // 401 status represents unauthorized
+      message: "Unauthorized user",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // now we verfiy the token, if token is tempered it will give error
+    // else it will return this
+    //   {
+    //     userID: user._id,
+    //     email: user.email,
+    //     username: user.username,
+    //   },
+
+    req.user = decoded;
+  } catch (error) {
+    return res.status(401).json({
+      // 401 status represents unauthorized
+      message: "Unauthorized user",
+    });
+  }
+
+  next(); //helped to ensure the middleware proceeds to the next step properly.
+}
+
+module.exports = auth;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,37 +5,40 @@ const upload = require("../config/multer.config");
 const supabase = require("../config/supabase.config");
 const fileModel = require("../model/files.model");
 
-router.get("/home", (req, res) => {
+const AuthMiddleWare = require("../middleware/auth");
+
+router.get("/home", AuthMiddleWare, (req, res) => {
   res.render("home");
-});
+}); // by using the middleware this page only renders if we logged in
 
 router.post("/upload", upload.single("file"), async (req, res) => {
-  res.send(req.file);
+  // file is the name of input in home ejs
+  console.log(req.file);
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  const { data, error } = await supabase.storage
+    .from("drive")
+    .upload("uploads/" + req.file.originalname, req.file.buffer, {
+      contentType: req.file.mimetype,
+    });
+
+  console.log(data);
+
+  if (error) {
+    return res.status(500).send(error.message);
+  }
+  const filePath = data.path;
+  const originalName = req.file.originalname;
+
+  const newFile = await fileModel.create({
+    path: filePath,
+    originName: originalName,
+  });
+
+  res.send({
+    message: "File uploaded successfully!",
+  });
 });
-
-// file is the name of input in home ejs
-
-// const { data, error } = await supabase.storage
-//   .from("drive")
-//   .upload("uploads/" + req.file.originalname, req.file.buffer, {
-//     contentType: req.file.mimetype,
-//   });
-
-// console.log(data);
-
-// if (error) {
-//   return res.status(500).send(error.message);
-// }
-// const filePath = data.path;
-// const originalName = req.file.originalname;
-
-// const newFile = await fileModel.create({
-//   path: filePath,
-//   originName: originalName,
-// });
-
-// res.send({
-//   message: "File uploaded successfully!",
-// });
 
 module.exports = router;
