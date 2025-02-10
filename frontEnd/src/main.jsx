40eae0f6e77740b662d5805297ac785b3639052d
DjayNii <nithinreddyk24@gmail.com>
2025-02-10 20:55:18 +0530

    changes due to netlify

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
-//@ts-ignore
-import App from "./App.jsx";
+
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
